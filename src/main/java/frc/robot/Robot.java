1e36de51e56fa5dbab8d21524ebedbcebed76600
ZCrAzYF <59585220+ZCrAzYF@users.noreply.github.com>
2021-02-27 14:29:58 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,12 +1,11 @@
 package frc.robot;
 
-//import edu.wpi.first.wpilibj.XboxController;
-//import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX; //imports the correct victor API
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 //import edu.wpi.first.wpilibj.SpeedController;
@@ -23,18 +22,24 @@ public class Robot extends TimedRobot
   // private MecanumDrive m_robotDrive;
 
    private Joystick m_stick;
+   private final XboxController m_driverController = new XboxController(0);
     //temp
    JoystickButton Button1 = new JoystickButton(m_stick, 2);
+
+   //joystick button setup VVV
+   //Trigger for shooting
+   //5 and 3 for intake controlles
+   //11 and 12 for cameras
 
    //sets up the ports of motors
    private static final int kMotorPortLeftFront = 10;
    private static final int kMotorPortLeftRear = 11; 
    private static final int kMotorPortRightFront = 20;
    private static final int kMotorPortRightRear = 21;
-   private static final int kMotorPortColorWH = 2; 
+   private static final int kMotorPortIntake = 30; 
    private static final int kMotorPortShooter = 3;
    private static final int kJoystickChannel = 0;
-
+   
    UsbCamera Front;
    UsbCamera back;
    UsbCamera shooter;
@@ -51,32 +56,23 @@ public class Robot extends TimedRobot
    WPI_VictorSPX m_RightRear = new WPI_VictorSPX(kMotorPortRightRear); 
    SpeedControllerGroup m_LeftGroup = new SpeedControllerGroup(m_LeftFront, m_LeftRear);
    SpeedControllerGroup m_RightGroup = new SpeedControllerGroup(m_RightFront, m_RightRear);
-   WPI_VictorSPX m_ColorWH = new WPI_VictorSPX(kMotorPortColorWH);
+   WPI_VictorSPX m_Intake = new WPI_VictorSPX(kMotorPortIntake);
    WPI_VictorSPX m_Shooter = new WPI_VictorSPX(kMotorPortShooter);
    DifferentialDrive m_drive = new DifferentialDrive(m_LeftGroup, m_RightGroup);
    double stick1val,stick2val;
-
-  Compressor air = new Compressor(); //sets up a compressor for pistons
+ 
   private final Timer m_timer = new Timer(); //identifys a timer 
 
 
 
 @Override
 public void robotInit(){
-    WPI_VictorSPX m_LeftFront = new WPI_VictorSPX(kMotorPortLeftFront);
-    WPI_VictorSPX m_RightFront = new WPI_VictorSPX(kMotorPortLeftRear);
-    WPI_VictorSPX m_LeftRear = new WPI_VictorSPX(kMotorPortRightFront);
-    WPI_VictorSPX m_RightRear = new WPI_VictorSPX(kMotorPortRightRear);
-    WPI_VictorSPX m_Shooter = new WPI_VictorSPX(kMotorPortShooter);
-    m_LeftFront.setInverted(true);
-    m_RightRear.setInverted(true);
-
+    
    Front = CameraServer.getInstance().startAutomaticCapture();
    back = CameraServer.getInstance().startAutomaticCapture();
    shooter = CameraServer.getInstance().startAutomaticCapture();
     server = CameraServer.getInstance().getServer();
-    //m_robotDrive = new MecanumDrive(m_leftFront, m_leftRear, m_rightFront, m_rightRear);
-
+    
     m_stick = new Joystick(kJoystickChannel);
 
 scaleFactor = 0.5;
@@ -106,16 +102,17 @@ public void autonomousPeriodic() {
 @Override
   public void teleopPeriodic() {
     //m_robotDrive.driveCartesian(m_stick.getX(), m_stick.getY(),m_stick.getZ(), 0.0)
-    m_drive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    //m_drive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    m_drive.tankDrive(m_driverController.getY(Hand.kLeft), m_driverController.getY(Hand.kRight));
 
-        if (m_stick.getRawButtonPressed(4)) 
+        if (m_driverController.getXButtonPressed()) 
         {
-          System.out.println("Setting camera 2");
+          System.out.println("Setting Back camera");
           server.setSource(back);
          } 
-      else if (m_stick.getRawButtonPressed(3)) 
+      else if (m_driverController.getAButtonPressed()) 
       {
-          System.out.println("Setting camera 1");
+          System.out.println("Setting Front camera");
           server.setSource(Front);
       }
       if (m_stick.getRawButtonPressed(2))
@@ -127,9 +124,30 @@ public void autonomousPeriodic() {
   
     m_drive.setSafetyEnabled(false);
 
+    //Runs intake on button 5 presed reverses with 3 
+   if(m_driverController.getBumperPressed(Hand.kRight))
+   {
+     m_Intake.set(0.75);
+   }
+   else if(m_driverController.getBumperReleased(Hand.kRight))
+   {
+      m_Intake.set(0);
+   }
+   if(m_driverController.getBumperPressed(Hand.kLeft))
+   {
+     m_Intake.set(-0.75);
+   }
+   else if(m_driverController.getBumperReleased(Hand.kLeft))
+   {
+      m_Intake.set(0);
+   }
+
+   //shooter
    
-    
- 
+  
+      m_Shooter.set(m_driverController.getTriggerAxis(Hand.kRight));
+   
+
   }
  
 
